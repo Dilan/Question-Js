520ab7b998f09a1e206d3a5b9a88ece3f9942457
Anton Plotnikov <antony@xiag.ch>
2012-06-08 10:17:35 +0700

    split suggestion into symbols and add onClick listener

--- public/js/game.js
+++ public/js/game.js
@@ -15,6 +15,25 @@ $(function() {
     });
 
     var Suggestion = Backbone.Model.extend({
+        defaults : {
+            uid     : null,
+            match   : new Match(),
+            value   : null,
+            symbols : null
+        }
+    });
+
+    var Symbol = Backbone.Model.extend({
+        defaults : {
+            uid   : null,
+            value : null,
+            color : null
+        }
+    });
+
+    var SymbolList = Backbone.Collection.extend({
+
+        model: Symbol
 
     });
 
@@ -22,9 +41,10 @@ $(function() {
 
         model: Suggestion,
 
-        isOrderExist: function(order) {
+        isSuggestionExist: function(uid) {
             return this.filter(
-                function(suggestion){ return suggestion.get('order') == order; }).length?true:false;
+                function(suggestion){ return suggestion.get('uid') == uid; }
+            );
         }
 
     });
@@ -39,6 +59,7 @@ $(function() {
         },
 
         parse:function(response) {
+
             var json = JSON.parse(
                 JSON.stringify(
                     response
@@ -51,21 +72,83 @@ $(function() {
                     json.question
                 )
             );
+            // parse method called when object [SAVE] and when [RESTORE] as a result we should not update suggestion list twice
             for(i=0; i<json.suggestionList.length; i++) {
-                if(false == this.get("suggestionList").isOrderExist(json.suggestionList[i].order)) {
-                    this.get("suggestionList").add(
-                        json.suggestionList[i]
-                    );
+                if(false == this.get("suggestionList").isSuggestionExist(json.suggestionList[i].uid)) {
+                    this.addSuggestion(json.suggestionList[i]);
                 }
             }
         },
 
-        createSuggestion:function(value) {
-            this.get("suggestionList").add({
-                value: value,
-                order: this.get("suggestionList").length
-            });
+        addSuggestion:function(suggestion) {
+            this.get("suggestionList").add(suggestion);
+        },
+
+        resetSuggestionList:function() {
+            this.get('suggestionList').reset();
             this.save();
+        },
+
+        createSuggestion:function(value) {
+            var suggestion = new Suggestion(
+                {
+                    match: this.getMatch(value),
+                    value: value
+                }
+            );
+            suggestion.set(
+                "uid",
+                suggestion.cid
+            );
+            suggestion.set(
+                "symbols",
+                this.createSymbolList(
+                    value,
+                    suggestion.cid
+                )
+            );
+
+            this.addSuggestion(suggestion);
+            this.save();
+        },
+
+        createSymbolList:function(value, uid) {
+            var symbol_list = new SymbolList;
+
+            for(var i=0; i<value.length; i++) {
+                var symbol = new Symbol(
+                    {
+                        value: value[i]
+                    }
+                );
+                // set UID into attribute to make possible to use [JSON.stringify]
+                symbol.set("uid", uid + '-' + symbol.cid);
+                symbol_list.add(symbol);
+            }
+
+            return symbol_list;
+        },
+
+        getMatch:function(value) {
+            var exact = 0; var partial = 0;
+            var question = this.get("question").get("value");
+
+            for(i=0; i<question.length; i++) {
+                if(question[i] == value[i]) {
+                    exact++;
+                }
+                else if(question.indexOf(value[i]) != -1 ) {
+                    partial++;
+                }
+            }
+
+            return new Match(
+                {
+                    exact:exact,
+                    partial:partial
+                }
+            );
+
         }
 
     });
@@ -88,7 +171,15 @@ $(function() {
         },
 
         render: function() {
-          this.$el.html(this.template(this.model.toJSON()));
+          this.$el.html(
+              this.template(
+                  JSON.parse(
+                      JSON.stringify(
+                          this.model
+                      )
+                  )
+              )
+          );
           return this;
         }
 
@@ -99,13 +190,14 @@ $(function() {
 
         events: {
             "keypress #new-suggestion":  "createOnEnter",
-            "click #button": "sample"
+            "click #reset": "resetSuggestionList"
         },
 
         initialize: function() {
             this.input = this.$("#new-suggestion");
             this.game = new Game({ id:1 });
 
+            // listener
             this.game.get('suggestionList').bind('add', this.addSuggestion, this);
 
             this.game.fetch();
@@ -116,18 +208,31 @@ $(function() {
             if (!this.input.val()) return;
 
             this.game.createSuggestion(this.input.val());
-
             this.input.val('');
         },
 
-        sample: function() {
-            this.game.get('suggestionList').reset();
-            this.game.save();
+        resetSuggestionList: function() {
+            this.game.resetSuggestionList();
+            this.$("#suggestion-list").html('');
         },
 
         addSuggestion: function(suggestion) {
             var view = new SuggestionView({model: suggestion});
             this.$("#suggestion-list").append(view.render().el);
+
+            // bind click
+            var thisGameView = this;
+            _.each(suggestion.get("symbols"), function(item) {
+                $('#'+item.uid).bind(
+                    'click',
+                    jQuery.proxy(thisGameView, 'changeColorOnClick')
+                );
+            });
+        },
+
+        changeColorOnClick: function(e) {
+            var id = e.target.id;
+            alert("You click to #"+id);
         }
 
     });
